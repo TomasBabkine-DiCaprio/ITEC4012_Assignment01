11b92e677336b3d61d5ee263079c0915b1f38f68
TomasBabkineDiCaprio <babkinedicapriotomas@gmail.com>
2021-10-05 09:47:18 -0400

    Initial commit

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const port = 8080;
 // API / urls
 // default:
 app.get('/', (req, res) => {
-    res.send("Hi, the express server is running.");
+    // Home page
+    const title = "<h1>Tomas Babkine-Di Caprio - ITEC 4012 - Assignment 01<br>101124714</h1>";
+
+    // format html
+    let formattedHTML = title;
+    // send the formatted HTML to be displayed on the webpage
+    res.send(formattedHTML);
 });
 
 // run the express http server on the port we specified
